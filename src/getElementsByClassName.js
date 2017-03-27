8211223f521ef4492dd1e8958da76e4f0da1c4c0
Hack Reactor Student <student@hackreactor.com>
2017-03-27 15:26:00 -0700

    Implement getElementsByClassName with own function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,31 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+
+
+	var elementArr = [];
+
+	function nodeTraverse(element) {
+	  
+	  for(var i = 0; i < element.childNodes.length; i++) {
+	    var nodeClasses = element.childNodes[i].classList;
+	    
+	    if(nodeClasses !== undefined && nodeClasses != null) {
+	    	var nodeArray = Array.from(element.childNodes[i].classList);
+	    
+	    	if (nodeArray.includes(className) && nodeArray) {
+	      		elementArr.push(element.childNodes[i]);
+	    	}
+	    }
+	    if(element.childNodes[i].childNodes.length !== 0) {
+	      nodeTraverse(element.childNodes[i]);
+	    }
+      }
+
+	}
+
+	nodeTraverse(document);
+
+	return elementArr;
+  
 };
