e654831b71e35b296107a4701e98363fe1ea2195
Hack Reactor Student <student@hackreactor.com>
2017-03-27 20:44:31 -0700

    Begin working on parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,50 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  if (json === 'null') {
+  	return null;
+  } else if (json === 'true') {
+  	return true;
+  } else if (json === 'false') {
+  	return false;
+  }
+
+  if (json.charAt(0) === '"' && json.charAt(json.length - 1) === '"') {
+  	return json.slice(1, json.length - 1);
+  }
+
+  if (json.charAt(0) === '[' && json.charAt(json.length - 1) === ']') {
+  	var jsonNoSpace = json.replace(/\s/g, '');
+  	var jsonSplitArr = jsonNoSpace.replace(/\[|\]/g, '').split(', ');
+
+  	if (json.length === 2) {
+		return [];
+	}
+
+  	return jsonSplitArr.map(ele => parseJSON(ele));
+  }
+
+  if(/(-?\d+.?\d*)/g.test(json)) {
+  	return parseFloat(json);
+  }
+
+  if (json.charAt(0) === '{' && json.charAt(json.length - 1) === '}') {
+  	var jsonObj = {};
+  	//var jsonNoSpace = json.replace(/\s/g, '');
+  	var jsonSplitArr = json.replace(/[{}]/g, '').match(/"\w+,\s?\w*":\s?\w+/g);
+
+	if (json.length === 2) {
+		return jsonObj;
+	}
+
+	for (var i = 0; i < jsonSplitArr.length; i++) {
+		var indexColon = jsonSplitArr[i].indexOf(':');
+		var prop = jsonSplitArr[i].slice(0, indexColon);
+		var val = jsonSplitArr[i].slice(indexColon + 1, jsonSplitArr[i].length);
+
+		jsonObj[parseJSON(prop)] = parseJSON(val);
+	}  	
+
+  	return jsonObj;
+  }
 };
